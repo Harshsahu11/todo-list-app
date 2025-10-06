14883e9a1b22a91f721f5f2d4154464340d338db
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-10-06 12:18:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [toDoList, setToDoList] = useState([]);
+  
+  let saveToDoList = (e) => {
+    let toName = e.target.toName.value;
+    
+    if(toDoList.includes(toName)){
+      alert("This task is already in the list");
+      return;
+    }
+    setToDoList([...toDoList, toName]);
+    input.value = "";
+    alert("Task added");
+    e.preventDefault();
+  }
+  let list = toDoList.map((value, index) => {
+    return (
+      <ToDoListItems value={value} key={index} indexNumber={index} 
+        toDoList={toDoList} setToDoList={setToDoList}
+      />
+    )
+}
+)
+  return (
+    <div className="App">
+      <h1>ToDo List</h1>
+      <form action="" onSubmit={saveToDoList}>
+        <input type="text" name='toName'/> 
+        <button>Save</button>
+      </form>
+      <div className='outerDiv'>
+        <ul>
+          {list}
+        </ul>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App;
+
+function ToDoListItems({value,indexNumber,toDoList,setToDoList}){
+
+  let deleteRow = () => {
+    let newList = toDoList.filter((item, index) => index !== indexNumber);
+    setToDoList(newList);
+    alert("Task deleted");
+  }
+  return (
+    <li >{indexNumber+1}. {value} <span onClick={deleteRow}>&times;</span></li>
+  )
+}
+
